c4df235cf473a16733d7d29a242eedb6fcc60d61
DiegoRG <diego.romang@uanl.edu.mx>
2026-03-25 16:54:49 -0600

    feat: implementacion de menu flotante para navegar a Nueva Nota o Nueva Contrasena

--- app/src/main/java/com/example/securenotes/PrincipalActivity.kt
+++ app/src/main/java/com/example/securenotes/PrincipalActivity.kt
@@ -1,6 +1,7 @@
 package com.example.securenotes
 
 import android.os.Bundle
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,32 @@ class PrincipalActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val fabNuevo = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.fabNuevo)
+
+        fabNuevo.setOnClickListener { vistaBoton ->
+            val contextoTema = android.view.ContextThemeWrapper(this, R.style.TemaMenuBoveda)
+
+            val popupMenu = android.widget.PopupMenu(contextoTema, vistaBoton)
+
+            popupMenu.menuInflater.inflate(R.menu.menu_opciones_nuevo, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { itemSeleccionado ->
+                when (itemSeleccionado.itemId) {
+                    R.id.opcion_nota -> {
+                        val intent = Intent(this, NuevaNotaActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    R.id.opcion_pass -> {
+                        val intent = Intent(this, NuevaContrasenaActivity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popupMenu.show()
         }
     }
 }
